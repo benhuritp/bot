d89fe41fe30bc52e1d8a9264b8caaca50b7ff629
benhuritp <153764064+benhuritp@users.noreply.github.com>
2024-07-13 03:32:54 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const urlPattern = /^(https?:\/\/[^\s/$.?#].[^\s]*)$/;
 let sourceLink = 'https://example.com/source';
 let showLinks = false; // Переменная для отслеживания состояния отображения ссылок
 
-const adminUsername = 'thebreot';
+const adminUsername = 'Kopylash8';
 
 let welcomeText = 'Ласкаво просимо! Наразі трансляцій відсутні.';
 let StreamsText = 'Ласкаво просимо! Щоб дивитись пряму трансляцію матчу, потрібно підписатись на всі канали нижче:';
