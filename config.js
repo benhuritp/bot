1134e8068616936b923ce41c29f982348e551826
benhuritp <153764064+benhuritp@users.noreply.github.com>
2024-07-13 03:28:12 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const config = {
-    "telegramToken": "7038635187:AAHWjcG48VQVPLhoIegGq3kl-_FEl4OYnng",
+    "telegramToken": "6361792538:AAGvVPUdeBHenFHW3Ksv9Xlh3AaZpSXdGdY",
     "catUrl":"",
     "weatherUrl":"", 
     "mongoUri":"mongodb+srv://thebreot:rtntn7Brt@suber.xlla4nj.mongodb.net/?retryWrites=true&w=majority&appName=suber"       
